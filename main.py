1453a07b90af73e95dafbb920b6f1eec9adda460
Mehsim Khurshid <mehsimkhurshid@Mehsims-MacBook-Pro.local>
2026-07-28 02:38:35 +0500

    Import database module globally at top of main.py to fix NameError

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ from volatility_clusterer import volatility_clusterer
 from gmm_trail import gmm_trailing_engine
 from garch_monitor import garch_vol_monitor
 from news_monitor import news_monitor
-from decay_calibrator import decay_calibrator
+import database
 from secret_manager import get_secure_env
+
 from bybit_client import (
 
     bybit_get_request,
